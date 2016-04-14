c34b2b4934c48587fcc2bf477ec64d9fc46e020e
Demond Childers <Demond@demonds-mbp.attlocal.net>
2016-04-13 23:35:04 -0400

    iReview- Restaurant Review app, which features four controls including slider, segment controller, stepper and switch.

--- iReview/ViewController.m
+++ iReview/ViewController.m
@@ -10,9 +10,36 @@
 
 @interface ViewController ()
 
+#pragma mark -Properties
+
+@property (nonatomic, weak) IBOutlet UILabel            *restLabel;
+@property (nonatomic, weak) IBOutlet UITextField        *nameTextField;
+@property (nonatomic, weak) IBOutlet UITextView         *reviewTextView;
+@property (nonatomic, weak) IBOutlet UISegmentedControl *foodSegControl;
+@property (nonatomic, weak) IBOutlet UIStepper          *foodStepper;
+@property (nonatomic, weak) IBOutlet UISlider           *overallSlider;
+@property (nonatomic, weak) IBOutlet UISwitch           *recomSwitch;
+
+
 @end
 
 @implementation ViewController
+
+
+#pragma mark -Interactivity Methods
+
+
+
+- (IBAction)setSubmitButton:(UIButton *)submitButton {
+    NSString *recString = @"";
+    if (_recomSwitch.isOn) {
+        recString = @"Yes";
+    } else {
+        recString = @"No";
+    }
+    NSLog(@"%@ Results by %@ review: %@, Food Rating: %@, Service: %i, Overall: %i, Recommended: %@",_restLabel.text, _nameTextField.text, _reviewTextView.text, [_foodSegControl titleForSegmentAtIndex:_foodSegControl.selectedSegmentIndex], (int)_foodStepper.value, (int)_overallSlider.value, recString);
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
